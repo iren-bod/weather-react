3e9aafb25e5ce64065df29313a0112b3a5eef772
Iryna Bodak <bodak.iryna@gmail.com>
2022-10-11 18:58:53 +0200

    fixed warnings

--- src/WeatherForecast/WeatherForecast.js
+++ src/WeatherForecast/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                 <DayForecast data={dailyForecast} />
               </div>
             );
+          } else {
+            return null;
           }
         })}
       </div>
